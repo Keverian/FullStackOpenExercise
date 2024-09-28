8683167dfdb6f29116109d42bdecdfad80570add
Keverian <zh632502683@gmail.com>
2024-09-28 00:12:59 -0700

    exericse 1.7 uniface step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -15,38 +15,60 @@ function PrintStat({text, counter}){
       <div>{text} {counter}</div>
     </>
   );
-};
-function HandleClick(){
-
 }
+
+function CalcAvg(good, bad, total){
+  return (good - bad)/total;
+}
+
+function calcPosPer(good, total){
+  return (good/total) * 100 + " %";
+}
+
 function App() {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [avg, setAvg] = useState(0);
+  const [posPer, setPosPer] = useState("0 %");
+  
   function HandleGoodButton(){
+    setAvg(CalcAvg(good + 1, bad, total + 1 ));
+    setPosPer(calcPosPer(good + 1, total + 1));
     setGood(good + 1);
+    setTotal(total + 1)
   }
-
+  
   function HandleBadButton(){
+    setPosPer(calcPosPer(good, total + 1));
+    setAvg(CalcAvg(good, bad + 1, total + 1 ));
     setBad(bad + 1);
+    setTotal(total + 1)
   }
-
+  
   function HandleNeutralButton(){
+    setPosPer(calcPosPer(good, total + 1));
+    setAvg(CalcAvg(good, bad, total + 1 ));
     setNeutral(neutral + 1);
+    setTotal(total + 1)
   }
 
+  
   return (
     <>
       <h1>give feedback</h1>
       <Button handleClick={HandleGoodButton} text='good'/>
-      <Button handleClick={HandleBadButton} text='neutral'/>
-      <Button handleClick={HandleNeutralButton} text='bad'/>
+      <Button handleClick={HandleNeutralButton} text='neutral'/>
+      <Button handleClick={HandleBadButton} text='bad'/>
       <h1>statistics</h1>
       <ul>
         <PrintStat text='good' counter={good}/>
         <PrintStat text='neutral' counter={neutral}/>
         <PrintStat text='bad' counter={bad}/>
+        <PrintStat text='total' counter={total}/>
+        <PrintStat text='avg' counter={avg}/>
+        <PrintStat text='positive' counter={posPer}/>
       </ul>
     </>
   )
